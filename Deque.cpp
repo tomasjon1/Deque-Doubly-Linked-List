ce9792de692087ea55231d206423e4e178043150
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2022-02-22 23:46:37 +0200

    Structures of Node and Deque

--- Deque.cpp
+++ Deque.cpp
@@ -1,20 +1,40 @@
-// Deque.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+// Node of a doubly linked list
+struct Node
+{
+    int data;
+    Node* prev;
+    Node* next;
+    // function to get new node with data and null pointers to next/prev node
+    static Node* getnode(int data)
+    {
+        Node* newNode = (Node*)malloc(sizeof(Node));
+        newNode->data = data;
+        newNode->prev = newNode->next = NULL;
+        return newNode;
+    }
+};
+
+//Class of deque
+class Deque
+{
+    Node* front;
+    Node* rear;
+    int Size;
+
+//Consturctor to create empty deque
+public:
+    Deque()
+    {
+        front = rear = NULL;
+        Size = 0;
+    }
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
